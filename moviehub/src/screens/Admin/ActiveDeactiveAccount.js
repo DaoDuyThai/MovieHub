b02ad722654e064c2fcc4b59b8f87e88b46541f1
Đào Duy Thái <daoduythai.business@gmail.com>
2023-07-10 10:23:40 +0700

    Finish merge Hieu

--- moviehub/src/screens/Admin/ActiveDeactiveAccount.js
+++ moviehub/src/screens/Admin/ActiveDeactiveAccount.js
@@ -1,16 +1,20 @@
 import React from "react";
 import LeftDashboard from "../../components/Admin/leftDashboard";
 import RightDashboard from "../../components/Admin/rightDashboard";
+import Header from "../../components/Header/Header";
+import Footer from "../../components/Footer/Footer";
 
 function ActiveDeactiveAccount() {
     return (
         <>
+            <Header />
             <React.Fragment>
                 <div style={{ display: 'flex' }}>
                     <LeftDashboard />
                     <RightDashboard />
                 </div>
             </React.Fragment>
+            <Footer />
         </>
     )
 }
