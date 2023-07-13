25640db1d66f135fc468f9cd12d12baa0ec13197
Đào Duy Thái <daoduythai.business@gmail.com>
2023-07-13 15:09:39 +0700

    Use session storage

--- moviehub/src/components/Header/Header.js
+++ moviehub/src/components/Header/Header.js
@@ -1,52 +1,74 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'font-awesome/css/font-awesome.min.css';
 import './Header.css'; // Import custom CSS file for styling
 
 const Header = () => {
-    const [showSearch, setShowSearch] = useState(false);
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const [username, setUsername] = useState('');
 
-    const handleSearchClick = () => {
-        setShowSearch(!showSearch);
-    };
+  useEffect(() => {
+    const loggedInStatus = sessionStorage.getItem('isLoggedIn');
+    const storedUsername = sessionStorage.getItem('username');
+    if (loggedInStatus === 'true' && storedUsername) {
+      setIsLoggedIn(true);
+      setUsername(storedUsername);
+    }
+  }, []);
 
-    return (
-        <nav className="navbar navbar-expand-lg navbar-dark navbar-background">
-            <div className="container-fluid">
-                <Link style={{fontWeight:"bold"}} className="navbar-brand text-light" to="/">
-                    MOVIE<span className="text-warning">HUB</span>
-                </Link>
+  const handleLogout = () => {
+    setIsLoggedIn(false);
+    setUsername('');
+    sessionStorage.removeItem('isLoggedIn');
+    sessionStorage.removeItem('username');
+  };
 
-                <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
-                    <div className="navbar-nav mr-auto">
-                        <Link className="nav-link" to="/movies">
-                            <span className="nav-link-text">Movies</span>
-                        </Link>
-                        <Link className="nav-link" to="/tvshow">
-                            <span className="nav-link-text">TV Shows</span>
-                        </Link>
-                        <Link className="nav-link" to="/">
-                            <span className="nav-link-text">About us</span>
-                        </Link>
-                        <Link className="nav-link" to="/admin">
-                            <span className="nav-link-text">Admin</span>
-                        </Link>
-                    </div>
-                </div>
-                <div className="d-flex align-items-center">
-                    
+  return (
+    <nav className="navbar navbar-expand-lg navbar-dark navbar-background">
+      <div className="container-fluid">
+        <Link style={{ fontWeight: 'bold' }} className="navbar-brand text-light" to="/">
+          MOVIE<span className="text-warning">HUB</span>
+        </Link>
 
-                    <Link className="nav-link" to="/login">
-                        <button className='btn btn-outline-warning button-left'>Sign In</button>
-                    </Link>
-                    <Link className="nav-link" to="/register">
-                        <button className='btn btn-outline-warning button-left'>Sign Up</button>
-                    </Link>
-                </div>
-            </div>
-        </nav>
-    );
+        <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
+          <div className="navbar-nav mr-auto">
+            <Link className="nav-link" to="/movies">
+              <span className="nav-link-text">Movies</span>
+            </Link>
+            <Link className="nav-link" to="/tvshow">
+              <span className="nav-link-text">TV Shows</span>
+            </Link>
+            <Link className="nav-link" to="/">
+              <span className="nav-link-text">About us</span>
+            </Link>
+            <Link className="nav-link" to="/admin">
+              <span className="nav-link-text">Admin</span>
+            </Link>
+          </div>
+        </div>
+        <div className="d-flex align-items-center">
+          {isLoggedIn ? (
+            <p className="text-light m-0 mr-2">Hello, {username}</p>
+          ) : (
+            <>
+              <Link className="nav-link" to="/login">
+                <button className="btn btn-outline-warning button-left">Sign In</button>
+              </Link>
+              <Link className="nav-link" to="/register">
+                <button className="btn btn-outline-warning button-left">Sign Up</button>
+              </Link>
+            </>
+          )}
+          {isLoggedIn && (
+            <button className="btn btn-outline-warning button-left" onClick={handleLogout}>
+              Logout
+            </button>
+          )}
+        </div>
+      </div>
+    </nav>
+  );
 };
 
 export default Header;
--- moviehub/src/components/Login/index.js
+++ moviehub/src/components/Login/index.js
@@ -1,53 +1,53 @@
-import { useState, useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import "./LoginContent.css";
-import { useNavigate } from "react-router";
 import { Link } from "react-router-dom";
 import { toast } from "react-toastify";
+
 function LoginContent() {
-    const [mail, setMail] = useState("");
-    const [password, setPassword] = useState("");
-    const [listAccount, setListAccount] = useState([]);
-    const [isUndifined, setIsUndifined] = useState(null);
-    const [isBlock, setIsBlock] = useState(false)
-    // const { account, setAccount } = useContext(AccountContext);
-    const navigate = useNavigate();
-  
-    useEffect(() => {
-      fetch(`http://localhost:8000/account`, {
-        method: "GET",
-        headers: { "Content-Type": "application/json" },
-      })
-        .then((response) => response.json())
-        .then((data) => {
-          setListAccount(data);
-        });
-    }, []);
-  
-    const handleLogin = () => {
-      const account = listAccount.find(
-        (a) => a.email === mail && a.password === password
-      );
-      if (account === undefined) {
-        setIsUndifined(false);
-        toast('Username or Password is Incorrect!!!')
-      } else if(account.active !== false){
-        // setAccount(account);
-        navigate("/");
-        toast("Login successfully!!");
-      }else{
-        setIsBlock(true)
-        toast('This account is Blocked!!')
-      }
-    };
-  
-    return (
-      <div className="boc">
+  const [mail, setMail] = useState("");
+  const [password, setPassword] = useState("");
+  const [listAccount, setListAccount] = useState([]);
+  const [isUndefined, setIsUndefined] = useState(null);
+  const [isBlocked, setIsBlocked] = useState(false);
+
+  useEffect(() => {
+    fetch("http://localhost:8000/account", {
+      method: "GET",
+      headers: { "Content-Type": "application/json" },
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        setListAccount(data);
+      });
+  }, []);
+
+  const handleLogin = () => {
+    const account = listAccount.find(
+      (a) => a.email === mail && a.password === password
+    );
+    if (account === undefined) {
+      setIsUndefined(false);
+      toast("Username or Password is Incorrect!!!");
+    } else if (account.active !== false) {
+      setIsUndefined(true);
+      sessionStorage.setItem("isLoggedIn", "true");
+      sessionStorage.setItem("username", account.name);
+      toast("Login successfully!!");
+      window.location.href = "/"; // Redirect to home page
+    } else {
+      setIsBlocked(true);
+      toast("This account is Blocked!!");
+    }
+  };
+
+  return (
+    <div className="boc">
       <div className="container">
         <div className="loginContent">
           <div className="leftContent">
             <h1>Đăng Nhập</h1>
             <img src="https://scontent-xsp1-3.xx.fbcdn.net/v/t1.6435-9/65120132_477890072962556_133111881295462400_n.jpg?_nc_cat=109&ccb=1-7&_nc_sid=730e14&_nc_ohc=mGF0BDlL5rkAX9_VSIn&_nc_ht=scontent-xsp1-3.xx&oh=00_AfDAbHIIbBTlWOzstgzZU2JSk_5iLeFaXFAtG255tSOnRQ&oe=64BFCBDD" />
-            <h2>Privacy policy {"&"} Term of service</h2>
+            <h2>Privacy policy & Term of service</h2>
           </div>
           <div className="rightContent">
             <div className="input">
@@ -62,25 +62,33 @@ function LoginContent() {
               <label htmlFor="password">Password:</label>
               <input
                 placeholder="Password"
-                type={"password"}
+                type="password"
                 id="password"
                 onChange={(e) => setPassword(e.target.value)}
               />
             </div>
-            {isUndifined === false ? (
+            {isUndefined === false && (
               <p
-                style={{ color: "#f9004d", textAlign: "left", fontSize: "12px" }}
+                style={{
+                  color: "#f9004d",
+                  textAlign: "left",
+                  fontSize: "12px",
+                }}
               >
                 Incorrect Email or Password
               </p>
-            ) : (
-              ""
             )}
-            {isBlock === true?<p
-                style={{ color: "#f9004d", textAlign: "left", fontSize: "12px" }}
+            {isBlocked === true && (
+              <p
+                style={{
+                  color: "#f9004d",
+                  textAlign: "left",
+                  fontSize: "12px",
+                }}
               >
                 Account is Blocked
-              </p>:''}
+              </p>
+            )}
             <div className="handle">
               <button onClick={handleLogin}>Đăng nhập</button>
               <a className="remember">Quên mật khẩu</a>
@@ -98,8 +106,8 @@ function LoginContent() {
           </div>
         </div>
       </div>
-      </div>
-    );
-  }
-  
-  export default LoginContent;
+    </div>
+  );
+}
+
+export default LoginContent;
